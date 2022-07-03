21a2df0a61ad8bf3173b8862696910081c3ab5a7
Gaurav95-g <gaurav.g95garg@gmail.com>
2022-07-03 23:53:38 +0530

    updated

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -1,18 +1,36 @@
-import logging
-from datetime import datetime
 import os
+import sys
 
-LOG_DIR="housing_dir"
+class HousingException(Exception):
+    
+    def __init__(self, error_message:Exception,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=HousingException.get_detailed_error_message(error_message=error_message,
+                                                                       error_detail=error_detail
+                                                                        )
 
-CURRENT_TIME_STAMP= f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 
-LOG_FILE_NAME=f"log_{CURRENT_TIME_STAMP}.log"
+    @staticmethod
+    def get_detailed_error_message(error_message:Exception,error_detail:sys)->str:
+        """
+        error_message: Exception object
+        error_detail: object of sys module
+        """
+        _,_ ,exec_tb = error_detail.exc_info()
+        exception_block_line_number = exec_tb.tb_frame.f_lineno
+        try_block_line_number = exec_tb.tb_lineno
+        file_name = exec_tb.tb_frame.f_code.co_filename
+        error_message = f"""
+        Error occured in script: 
+        [ {file_name} ] at 
+        try block line number: [{try_block_line_number}] and exception block line number: [{exception_block_line_number}] 
+        error message: [{error_message}]
+        """
+        return error_message
 
-os.makedirs(LOG_DIR)
+    def __str__(self):
+        return self.error_message
 
-LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
 
-logging.basicConfig(filename=LOG_FILE_PATH,
-filemode="w",
-format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
-level=logging.INFO  )
+    def __repr__(self) -> str:
+        return HousingException.__name__.str()
